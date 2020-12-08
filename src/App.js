7ae9a30a09ed8a543a463857849c046fc09604bd
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2020-12-08 12:07:46 +0530

    Conditional JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
 import "./styles.css";
 
-var shoppingList = ["milk","eggs","flowers","bread"]
+var shoppingList = ["milk", "eggs", "flowers", "bread"];
 export default function App() {
+  function getBG(index) {
+    if (index % 2 === 0) {
+      return "white";
+    }
+    return "gray";
+  }
+
+  function getPad(index) {
+    if (index % 2 === 0) {
+      return "0rem";
+    }
+    return "1rem";
+  }
   return (
     <div className="App">
       <h1>Print my shopping List</h1>
       <ul>
         {/*
-        without javascript
+        with javascript
         <li>{shoppingList[0]}</li>
         <li>{shoppingList[1]}</li>
         <li>{shoppingList[2]}</li>
         <li>{shoppingList[3]}</li>
       */}
-      {shoppingList.map(function(item){
-        return <li>{item}</li>
-      })}
-        </ul> 
-      </div>
+        {shoppingList.map(function (item, index) {
+          //  Conditional JSX
+          return (
+            <li
+              style={{ backgroundColor: getBG(index), padding: getPad(index) }}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
